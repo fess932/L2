632db0306cb3daa0083fdf54f07d4f18b2cf1dc8
Ivan Cheremisin <fess932@gmail.com>
2022-05-31 11:00:51 +0300

    l1

--- develop/dev01/task.go
+++ develop/dev01/task.go
@@ -1,5 +1,30 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"github.com/beevik/ntp"
+	"os"
+	"time"
+)
 
+// Создать программу печатающую точное время с использованием NTP -библиотеки. Инициализировать как go module.
+// Использовать библиотеку github.com/beevik/ntp.
+// Написать программу печатающую текущее время / точное время с использованием этой библиотеки.
+//
+// Требования:
+// Программа должна быть оформлена как go module
+// Программа должна корректно обрабатывать ошибки библиотеки:
+// выводить их в STDERR и возвращать ненулевой код выхода в OS
+
+func main() {
+	t, err := ntp.Time("0.ru.pool.ntp.org")
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+
+		return
+	}
+
+	fmt.Println(t)
+	fmt.Println(time.Now())
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,9 @@
 module l2
 
 go 1.18
+
+require (
+	github.com/beevik/ntp v0.3.0 // indirect
+	golang.org/x/net v0.0.0-20220526153639-5463443f8c37 // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
+)

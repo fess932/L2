ee5d4dfa5c7650efc2975b7d2ddb4a6ba3eb900a
Ivan Cheremisin <fess932@gmail.com>
2022-05-31 17:25:42 +0300

    update

--- develop/dev02/task.go
+++ develop/dev02/task.go
@@ -1,5 +1,66 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
+	"strconv"
+	"strings"
+)
 
+/*
+=== Задача на распаковку ===
+Создать Go функцию, осуществляющую примитивную распаковку строки, содержащую повторяющиеся символы / руны, например:
+	- "a4bc2d5e" => "aaaabccddddde"
+	- "abcd" => "abcd"
+	- "45" => "" (некорректная строка)
+	- "" => ""
+Дополнительное задание: поддержка escape - последовательностей
+	- qwe\4\5 => qwe45 (*)
+	- qwe\45 => qwe44444 (*)
+	- qwe\\5 => qwe\\\\\ (*)
+В случае если была передана некорректная строка функция должна возвращать ошибку. Написать unit-тесты.
+Функция должна проходить все тесты. Код должен проходить проверки go vet и golint.
+*/
+
+func main() {
+	log.Println(unpack("a1b2"))
+}
+
+func unpack(s string) (string, error) {
+	const nums = "1234567890"
+
+	var (
+		b           strings.Builder
+		num, letter string
+		letters     = strings.Split(s, "")
+	)
+
+	if len(letters) < 2 {
+		return s, nil
+	}
+
+	for _, v := range letters {
+		if strings.Contains(nums, v) {
+			num += v
+
+			continue
+		}
+
+		if num == "" {
+			b.WriteString(letter)
+
+			continue
+		}
+
+		count, err := strconv.Atoi(num)
+		if err != nil {
+			return "", fmt.Errorf("invalid string: %w", err)
+		}
+
+		b.WriteString(strings.Repeat(letter, count))
+
+		letter = v
+	}
+
+	return b.String(), nil
 }
--- develop/dev02/task_test.go
+++ develop/dev02/task_test.go
@@ -1,1 +1,38 @@
 package main
+
+import (
+	"testing"
+)
+
+func Test_unpack(t *testing.T) {
+
+	tcs := []struct {
+		input  string
+		output string
+	}{
+		{"a", "a"},
+		{"a2b3c", "aabbbc"},
+		{"a2b3c4d", "aabbbccccd"},
+	}
+
+	t.Parallel()
+
+	for _, tc := range tcs {
+		tc := tc
+
+		t.Run(tc.input, func(t *testing.T) {
+			t.Parallel()
+
+			out, err := unpack(tc.input)
+			if err != nil {
+				t.Errorf("\nunpack(%s)\nerror: %v", tc.input, err)
+
+				return
+			}
+
+			if out != tc.output {
+				t.Errorf("\nunpack(%v) = %v\nwant %v", tc.input, out, tc.output)
+			}
+		})
+	}
+}

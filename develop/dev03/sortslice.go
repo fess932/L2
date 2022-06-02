e84ceb912a3226a9a99982c30d3ce1539af6e9ef
Ivan Cheremisin <fess932@gmail.com>
2022-06-02 23:04:18 +0300

    3 first 4 questions work, now with star

--- develop/dev03/sortslice.go
+++ develop/dev03/sortslice.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"io"
 	"sort"
+	"strconv"
 	"strings"
 )
 
@@ -40,3 +41,134 @@ type SortSlice []SortRow
 func (x SortSlice) Len() int           { return len(x) }
 func (x SortSlice) Less(i, j int) bool { return x[i].String() < x[j].String() }
 func (x SortSlice) Swap(i, j int)      { x[i], x[j] = x[j], x[i] }
+
+///////////////////////////////////////////
+// sort options
+///////////////////////////////////////////
+
+type SortOption func(sort.Interface) sort.Interface
+
+func withReverse() SortOption {
+	return sort.Reverse
+}
+
+////////////////////////////////////////
+
+func withUniq() SortOption {
+	return Uniq
+}
+
+// Uniq input sorted slice, returns slice with Length() == all uniq values
+// calc uniq slice, set max len = last uniq index
+func Uniq(data sort.Interface) sort.Interface {
+	sort.Sort(data)
+
+	length := data.Len()
+	if length < 2 {
+		return data
+	}
+
+	i, j := 0, 1
+
+	// find the first duplicate
+	for j < length && data.Less(i, j) {
+		i++
+		j++
+	}
+
+	// this loop is simpler after the first duplicate is found
+	for ; j < length; j++ {
+		if data.Less(i, j) {
+			i++
+			data.Swap(i, j)
+		}
+	}
+
+	return &uniq{i + 1, data}
+}
+
+type uniq struct {
+	lastUniq int
+	sort.Interface
+}
+
+func (u uniq) Len() int {
+	return u.lastUniq
+}
+
+////////////////////////////////////////
+func withColumnNumber(k int) SortOption {
+	return func(s sort.Interface) sort.Interface {
+		return &columnSorter{k - 1, s.(SortSlice)}
+	}
+}
+
+type columnSorter struct {
+	column int
+	SortSlice
+}
+
+func (s columnSorter) Less(i, j int) bool {
+	var str1, str2 string
+
+	if len(s.SortSlice[i]) > s.column {
+		str1 = s.SortSlice[i][s.column]
+	}
+
+	if len(s.SortSlice[j]) > s.column {
+		str2 = s.SortSlice[j][s.column]
+	}
+
+	return str1 < str2
+}
+
+////////////////////////////////////////
+
+//-n — сортировать по числовому значению
+func withNumber(column int) SortOption {
+	return func(s sort.Interface) sort.Interface {
+		return &numberSorter{column - 1, s.(SortSlice)}
+	}
+}
+
+type numberSorter struct {
+	column int
+	SortSlice
+}
+
+func (s numberSorter) Less(i, j int) bool {
+	var num1, num2 int
+
+	if len(s.SortSlice[i]) > s.column {
+		num1 = parseInt(s.SortSlice[i][s.column])
+	}
+
+	if len(s.SortSlice[j]) > s.column {
+		num2 = parseInt(s.SortSlice[j][s.column])
+	}
+
+	return num1 < num2
+}
+
+// parseInt parse bad string with first number chars to int
+// "9asd" > "9"
+func parseInt(str string) int {
+	total := ""
+	for _, c := range str {
+		// todo: else "-" for negative numbers
+		if !strings.ContainsAny(string(c), "0123456789") {
+			break
+		}
+
+		total += string(c)
+	}
+
+	num, _ := strconv.Atoi(total)
+
+	return num
+}
+
+//-M — сортировать по названию месяца
+//-b — игнорировать хвостовые пробелы
+//-c — проверять отсортированы ли данные
+//-h — сортировать по числовому значению с учётом суффиксов
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -13,89 +13,6 @@ const data1 = `3 three two 10
 2 two one
 `
 
-func TestLinuxSortOptions(t *testing.T) {
-	tcs := []struct {
-		name, in, out string
-		flags         []SortOption
-	}{
-		{
-			"simple sort", data1,
-			`1 one two 2
-2 two one
-2 two one
-3 three two 10
-`, nil,
-		},
-
-		{
-			"reverse sort", data1, `3 three two 10
-2 two one
-2 two one
-1 one two 2
-`, []SortOption{withReverse()},
-		},
-
-		{
-			"uniq sort", data1,
-			`1 one two 2
-2 two one
-3 three two 10
-`,
-			[]SortOption{withUniq()},
-		},
-
-		{
-			"uniq reverse sort", data1,
-			`3 three two 10
-2 two one
-1 one two 2
-`,
-			[]SortOption{withUniq(), withReverse()},
-		},
-
-		{
-			"sort k column number", data1,
-			`2 two one
-2 two one
-3 three two 10
-1 one two 2
-`,
-			[]SortOption{withColumnNumber(4)},
-		},
-		{
-			"sort k column number rever", data1,
-			`1 one two 2
-3 three two 10
-2 two one
-`,
-			[]SortOption{withColumnNumber(4), withReverse(), withUniq()},
-		},
-		{
-			"sort by number value and column 4", data1,
-			`2 two one
-2 two one
-1 one two 2
-3 three two 10
-`,
-			[]SortOption{withNumber(4)},
-		},
-	}
-
-	t.Parallel()
-
-	for _, v := range tcs {
-		v := v
-
-		t.Run(v.name, func(t *testing.T) {
-			t.Parallel()
-
-			w := &strings.Builder{}
-			linuxSortOptions(strings.NewReader(v.in), w, v.flags...)
-			require.Equal(t, v.out, w.String())
-		})
-	}
-}
-
 func TestSortSlice(t *testing.T) {
 	tcs := []struct {
 		name, in, out string
@@ -160,10 +77,14 @@ func TestSortSlice(t *testing.T) {
 			[]SortOption{withColumnNumber(4), withReverse(), withUniq()},
 		},
 		{
-			"sort by number value and column 4", data1,
+			"sort by number value and column 4",
+			`3 three two 10
+1 one two 2s s
+2 two one
+2 two one`,
 			`2 two one
 2 two one
-1 one two 2
+1 one two 2s s
 3 three two 10
 `,
 			[]SortOption{withNumber(4)},

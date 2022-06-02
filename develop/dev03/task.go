a43b1da28625c176a415dd8ce24232300531e536
fess932 <fess932@gmail.com>
2022-06-02 08:29:40 +0300

    upd

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"io"
 	"log"
 	"os"
 	"sort"
@@ -30,17 +31,56 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	defer f.Close()
 
-	slice := make([]string, 0, 50)
+	linuxSort(f, os.Stdout)
+}
 
-	buf := bufio.NewScanner(f)
+const (
+	useK = iota + 1 //-k — указание колонки для сортировки
+	useN            //-n — сортировать по числовому значению
+	useR            //-r — сортировать в обратном порядке
+	useU            //-u — не выводить повторяющиеся строки
+)
+
+type Slice []string
+
+func (s Slice) Len() int {
+	return len(s)
+}
+
+func (s Slice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s Slice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+func linuxSort(r io.Reader, w io.Writer, args ...int) {
+	slice := make(Slice, 0, 50)
+
+	buf := bufio.NewScanner(r)
 	for buf.Scan() {
 		slice = append(slice, buf.Text())
 	}
 
-	sort.Strings(slice)
+	{
+		sort.Strings(slice)
+
+		for _, v := range args {
+			switch v {
+			case useK:
+			case useR:
+				slice = sort.Reverse(&slice).(Slice)
+			default:
+				log.Println("unknown option", v)
+			}
+		}
+
+	}
 
 	for _, v := range slice {
-		os.Stdout.WriteString(v + "\n")
+		io.WriteString(w, v+"\n") //nolint:errcheck
 	}
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+const data1 = `3 three two
+1 one two
+2 two one
+`
+
+const data2 = `1 one two
+2 two one
+3 three two
+`
+
+func TestLinuxSort(t *testing.T) {
+	w := &strings.Builder{}
+
+	linuxSort(
+		strings.NewReader(data1),
+		w,
+	)
+
+	require.Equal(t, w.String(), data2)
+}

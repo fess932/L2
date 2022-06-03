c43c349b661b3e066aa038eda8b1c3b48b097114
Ivan Cheremisin <fess932@gmail.com>
2022-06-03 15:23:44 +0300

    upd 9

--- develop/dev09/task.go
+++ develop/dev09/task.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"github.com/gocolly/colly"
-	"io"
-	"net/http"
+	"log"
 	"os"
 	"path"
+	"path/filepath"
+
+	"net/url"
+
+	"github.com/gocolly/colly/v2"
 )
 
 /*
@@ -20,27 +23,115 @@ func main() {
 
 // flag -r - рекурсивно скачивать все ссылки на сайте с тем же доменом
 
-func wget(url string, p string) {
-	w, err := http.NewRequest("GET", url, nil)
+func wget(turl string, p string) {
+	p, err := filepath.Abs(p)
 	if err != nil {
-		panic(err)
+		log.Fatal(err)
 	}
+	// w, err := http.NewRequest("GET", urgol, nil)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	r, err := http.DefaultClient.Do(w)
-	if err != nil {
-		panic(err)
-	}
+	// r, err := http.DefaultClient.Do(w)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		panic(err)
-	}
+	// body, err := io.ReadAll(r.Body)
+	// if err != nil {
+	// 	panic(err)
+	// }
 
-	os.WriteFile(path.Join(p, "index.html"), body, 0644)
+	// os.WriteFile(path.Join(p, "index.html"), body, 0644)
 
 	c := colly.NewCollector()
 
-	log.Prinlnt(c)
+	c.OnHTML("link[href]", func(e *colly.HTMLElement) {
+		next := e.Attr("href")
+		if next == "nil" {
+			return
+		}
+
+		u, err := url.Parse(next)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if u.IsAbs() {
+			return
+		}
+
+		e.Request.Visit(next)
+	})
+
+	c.OnHTML("script[src]", func(e *colly.HTMLElement) {
+		next := e.Attr("src")
+		if next == "nil" {
+			return
+		}
+
+		u, err := url.Parse(next)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if u.IsAbs() {
+			return
+		}
+
+		e.Request.Visit(next)
+	})
+
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		next := e.Attr("href")
+		if next == "nil" {
+			return
+		}
+
+		u, err := url.Parse(next)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+
+		if u.IsAbs() {
+			return
+		}
+
+		e.Request.Visit(next)
+	})
+
+	c.OnRequest(func(r *colly.Request) {
+		// log.Println("Visiting", r.URL)
+	})
+
+	c.OnResponse(func(r *colly.Response) {
+		u := r.Request.URL
+		log.Println("Visiting", u, u.Path)
+
+		dirPath := path.Join(p, u.Path)
+		os.MkdirAll(dirPath, os.ModePerm)
+
+		// TODO:
+		// check stylesheet (end on .css)
+		// js (end on .js)
+		// check any with .* on end
+		// default index.html
+
+		log.Println("FILENAME", r.FileName(), r.Headers)
+		if err := r.Save(dirPath + "/index.html"); err != nil {
+			log.Fatal(err)
+		}
+
+		// if err := os.WriteFile(dirPath+"/index.html", r.Body, os.ModePerm); err != nil {
+		// log.Println(err)
+		// }
+	})
+
+	c.Visit(turl)
 
 	//2 вида ссылок
 	//абсолютные

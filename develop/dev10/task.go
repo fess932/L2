d0e1047c292e10d92b8861dab8f09049189d3a02
Ivan Cheremisin <fess932@gmail.com>
2022-06-08 00:18:05 +0300

    10 upd

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -1,10 +1,22 @@
 package main
 
+import (
+	"bufio"
+	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
 /*
 === Утилита telnet ===
 Реализовать примитивный telnet клиент:
 Примеры вызовов:
-go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3s 1.1.1.1 123
+go-telnet --timeout=10s host port
+go-telnet mysite.ru 8080
+go-telnet --timeout=3s 1.1.1.1 123
 Программа должна подключаться к указанному хосту (ip или доменное имя) и порту по протоколу TCP.
 После подключения STDIN программы должен записываться в сокет, а данные полученные и сокета должны выводиться в STDOUT
 Опционально в программу можно передать таймаут на подключение к серверу (через аргумент --timeout, по умолчанию 10s).
@@ -12,7 +24,38 @@ go-telnet --timeout=10s host port go-telnet mysite.ru 8080 go-telnet --timeout=3
 Если сокет закрывается со стороны сервера, программа должна также завершаться.
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
 */
+// towel.blinkenlights.nl
 
 func main() {
+	//log.Println(gtelnet("towel.blinkenlights.nl:23"))
 
+	flag.Parse()
+	tf := flag.String("timeout", "10s", "timeout")
+
+	timeout, err := time.ParseDuration(*tf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), timeout)
+	defer cancel()
+
+	log.Println(gtelnet(ctx, "blabla.com:1234"))
+}
+
+func gtelnet(ctx context.Context, addr string) error {
+	var dialer net.Dialer
+
+	conn, err := dialer.DialContext(ctx, "tcp", addr)
+	if err != nil {
+		return fmt.Errorf("dial error: %w", err)
+	}
+
+	scanner := bufio.NewScanner(conn)
+
+	for scanner.Scan() {
+		fmt.Println(scanner.Text())
+	}
+
+	return nil
 }

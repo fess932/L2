564eaa2b73934f435ec79cd08d9217d171f30ab4
Ivan Cheremisin <fess932@gmail.com>
2022-06-08 11:32:13 +0300

    10 ok

--- develop/dev10/task.go
+++ develop/dev10/task.go
@@ -7,6 +7,7 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"time"
 )
 
@@ -23,39 +24,72 @@ go-telnet --timeout=3s 1.1.1.1 123
 При нажатии Ctrl+D программа должна закрывать сокет и завершаться.
 Если сокет закрывается со стороны сервера, программа должна также завершаться.
 При подключении к несуществующему сервер, программа должна завершаться через timeout.
+
+towel.blinkenlights.nl // 23 port default
+
+nc -l 1234 -> localhost:1234
+
 */
-// towel.blinkenlights.nl
 
 func main() {
-	//log.Println(gtelnet("towel.blinkenlights.nl:23"))
-
-	flag.Parse()
 	tf := flag.String("timeout", "10s", "timeout")
+	flag.Parse()
 
 	timeout, err := time.ParseDuration(*tf)
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	addr := ""
+
+	if flag.Arg(0) == "" {
+		log.Fatal("no host")
+	}
+
+	addr += flag.Arg(0)
+
+	if flag.Arg(1) == "" {
+		addr += ":23"
+	} else {
+		addr += ":" + flag.Arg(1)
+	}
+
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
 
-	log.Println(gtelnet(ctx, "blabla.com:1234"))
+	log.Println(gtelnet(ctx, cancel, addr))
 }
 
-func gtelnet(ctx context.Context, addr string) error {
+func gtelnet(ctx context.Context, cancel context.CancelFunc, addr string) error {
 	var dialer net.Dialer
 
 	conn, err := dialer.DialContext(ctx, "tcp", addr)
 	if err != nil {
 		return fmt.Errorf("dial error: %w", err)
 	}
+	defer conn.Close()
 
-	scanner := bufio.NewScanner(conn)
+	// write
+	go func() {
+		scanner := bufio.NewScanner(os.Stdin)
+		for scanner.Scan() {
+			conn.Write([]byte(scanner.Text() + "\n"))
+		}
 
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
+		cancel()
+	}()
+
+	// read
+	go func() {
+		scanner := bufio.NewScanner(conn)
+		for scanner.Scan() {
+			fmt.Println(scanner.Text())
+		}
+
+		cancel()
+	}()
+
+	<-ctx.Done()
 
 	return nil
 }

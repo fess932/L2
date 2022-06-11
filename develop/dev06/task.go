af9b66af33c16d70ae7364cdb01827f248b0abba
Ivan Cheremisin <fess932@gmail.com>
2022-06-11 18:37:02 +0300

    work at 6

--- develop/dev06/task.go
+++ develop/dev06/task.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"bufio"
+	"flag"
+	"io"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
 /*
 === Утилита cut ===
 Принимает STDIN, разбивает по разделителю (TAB) на колонки, выводит запрошенные
@@ -11,5 +21,60 @@ package main
 */
 
 func main() {
+	f := flag.String("f", "", "fields to cut, requered. example: -f 1,2,3")
+	d := flag.String("d", "\t", "delimiter, default is TAB")
+	s := flag.Bool("s", false, "separated - show only lines with delimiter")
 
+	flag.Parse()
+
+	if *f == "" {
+		log.Fatalln("fields is required")
+	}
+
+	fields, err := parseFields(*f)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	cut(os.Stdin, os.Stdout, fields, *d, *s)
+}
+
+// parseField parse field number from string
+// -3 > 1,2,3
+// 2- > 2,3
+// 1-2 > 1,2,3
+// 1,2 > 1,2
+func parseFields(str string) ([]int, error) {
+	raw := strings.Split(str, ",")
+	fields := make([]int, len(raw))
+
+	var err error
+	for i, field := range raw {
+		fields[i], err = strconv.Atoi(field)
+		if err != nil {
+			return nil, err
+		}
+
+		// absolute field number
+		if fields[i] < 0 {
+			fields[i] = -fields[i]
+		}
+	}
+
+	return fields, nil
+}
+
+func cut(r io.Reader, w io.Writer, fields []int, delimiter string, separated bool) {
+	s := bufio.NewScanner(r)
+	for s.Scan() {
+		columns := strings.Split(s.Text(), delimiter)
+		for _, field := range fields {
+			if field > len(columns) {
+				log.Println("Fields must be less than columns") // todo: remove this line
+				continue
+			}
+
+		}
+
+	}
 }

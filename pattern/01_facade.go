bb5bdca2de46e54b7c7c91e338029a621b9ce0a5
Ivan Cheremisin <fess932@gmail.com>
2022-06-12 09:43:42 +0300

    patterns 1-2

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -1,7 +1,58 @@
 package pattern
 
+import "net/http"
+
 /*
 	Реализовать паттерн «фасад».
 Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
+
+Скрытие сложности, унификация доступа к сложной системе.
+Антипаттерн - становление god object
 */
+
+// Facade - предоставляет простой интерфейс для сложной системы.
+// Пример на вводе строки в поисковую строку браузера.
+func FacadeSearch() {
+	// обработка ввода текста
+	searchString := typing()
+
+	// создание http запроса
+	req := createHttpRequest(searchString)
+
+	// отправка запроса на сервер
+	resp := sendHttpRequest(req)
+
+	// обработка ответа от сервера
+	view := processHttpResponse(resp)
+
+	// вывод ответа на экран
+	view.Show()
+}
+
+func mouseClick(target string) {
+
+}
+
+func typing() string {
+	return ""
+}
+
+func createHttpRequest(query string) *http.Request {
+	return nil
+}
+
+func sendHttpRequest(request *http.Request) *http.Response {
+	return nil
+}
+
+func processHttpResponse(response *http.Response) View {
+	return View{}
+}
+
+type View struct {
+}
+
+func (v View) Show() {
+
+}
--- pattern/02_builder.go
+++ pattern/02_builder.go
@@ -1,7 +1,57 @@
 package pattern
 
+import (
+	"io"
+	"os"
+)
+
 /*
 	Реализовать паттерн «строитель».
 Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Builder_pattern
+
+
+построение обьекта с помощью вспомогательных функций
+в го часто применяется, когда нужно построить обьект по разным параметрам
+например логгеры
 */
+
+func BuilderUsage() {
+	log := NewBuilder().
+		SetLevel(1).
+		SetName("my logger").
+		SetOutput(os.Stdout).
+		SetFormatter(func() {}).
+		Logger()
+	log.Log("Hello world")
+}
+
+type Logger struct {
+}
+
+func (l *Logger) Log(str string) {}
+
+type LoggerBuilder struct {
+	l *Logger
+}
+
+func (l *LoggerBuilder) Logger() *Logger {
+	return l.l
+}
+
+func NewBuilder() *LoggerBuilder {
+	return &LoggerBuilder{}
+}
+
+func (l *LoggerBuilder) SetLevel(level int) *LoggerBuilder {
+	return l
+}
+func (l *LoggerBuilder) SetName(name string) *LoggerBuilder {
+	return l
+}
+func (l *LoggerBuilder) SetOutput(output io.Writer) *LoggerBuilder {
+	return l
+}
+func (l *LoggerBuilder) SetFormatter(func()) *LoggerBuilder {
+	return l
+}

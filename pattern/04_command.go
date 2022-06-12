00f1fad2dcf1b045db7d5f57917261966f749459
Ivan Cheremisin <fess932@gmail.com>
2022-06-12 11:39:46 +0300

    patterns 4

--- pattern/04_command.go
+++ pattern/04_command.go
@@ -6,4 +6,44 @@ package pattern
 	https://en.wikipedia.org/wiki/Command_pattern
 
 Превращение запросов в обьекты
+Самый частый пример использования - кнопки в интерфейсе пользователя.
 */
+
+func commandUsage() {
+	send := SendButton{}
+	save := SaveButton{}
+
+	runCommand(send)
+	runCommand(save)
+}
+
+type Clicker interface {
+	Click()
+}
+
+func runCommand(click Clicker) {
+	click.Click()
+}
+
+// Типа абстрактный класс
+type Button struct{}
+
+func (b *Button) Click() {
+	println("действие по умолчанию")
+}
+
+type SendButton struct {
+	Button
+}
+
+func (sb SendButton) Click() {
+	println("отправка сообщения")
+}
+
+type SaveButton struct {
+	Button
+}
+
+func (sb SaveButton) Click() {
+	println("сохранение сообщения")
+}
